3da0a2b34481af006ee7adcfcbcd68c27cb5cd32
Sean <jesky.sean@outlook.com>
2017-11-22 19:53:22 -0700

    Update Ref_Passing_vector.cpp
    bugs should be all gone at this point, made some stupid mistakes but thats the best way to lean sometimes,... after a few days i finally got it to work just as id like it to. for now, when i keep getting better, ill keep adding some things here and there. but for right now its clean of bugs.

--- Ref_Passing_vector.cpp
+++ Ref_Passing_vector.cpp
@@ -7,63 +7,66 @@
  */
 #include <inclds>
 #include<exception>
+//[i like this equation] i / (32 + 7) + 2 + (i * i) / 3 << endl;
 
-double &comp_vec(vector<double>&v1, vector<double>&v2) 
-{
-    if (v1.empty() || v2.empty()) 
-    {
-        throw std::runtime_error("Restart your program; you have an empty vector that cannot be used in function: "
-                                "comp_vec.");
-    }
-
-    double v1_cnt = 0, v2_cnt = 0; //declare v1 & v2 to hold total additions
-    double v1_tot = 0, v2_tot = 0; //declare v1_t and v2_t to hold total avg's.
-    double j = 0;                  // j is to hold count for separate vector.    
-
-    if (v2.size() > v1.size());
-    {
-        for (decltype(v1.size()) i = 0; i != v1.size()
-                && j != v2.size(); ++i, ++j) 
-        {
-            v2_cnt += v2[j];
-            v1_cnt += v1[i];
-
+void return_some_numbers() {
+    /*
+     * change a few numbers and stuff to get some "random" numbers 
+     * to use as testing numbers for the function -
+     * "comp_vec(const &vec, const vec&);" 
+     */
+    
+        // using this to just place into calculator (in linux of course to easily check numbers;
+        for (int i = 0; i != 12; ++i) {
+            //cout << i + 0.1 / (32 + 7) + 2.3 + (i * i) / 3.5 << ", ";
+            //cout << i + 0.1 / (32 + 7) + 2.3 + (i * i) / 3.5 << " + ";
+            //cout << i * i / (32.8 + 7.1) + 2 + (i * i) / 0.2 << ", ";
+            //cout << i * i / (32.8 + 7.1) + 2 + (i * i) / 0.2 << " + ";
+           
         }
-    }
-    if (v1.size() > v2.size())
-    {
-        for (decltype(v2.size()) i = 0; i != v2.size()
-                && j != v1.size(); ++i, ++j) 
-        {
-            v1_cnt += v1[j];
-            v2_cnt += v2[i];
-        }
-    } 
-    else
-    {
-        for (decltype(v1.size()) i = 0; i != v1.size()
-                && j != v2.size(); ++i, ++j) 
-        {
-            v1_cnt += v1[i];
-            v2_cnt += v2[j];
-        }
-    }
-    //get avg's form both vectors.
-    v1_tot = v1_cnt / v1.size();
-    v2_tot = v2_cnt / v2.size();
-
-    //return the vector that has largest avg.
-    return v1_tot >= v2_tot ? v1_tot : v2_tot;
+   
 }
 
-int main() {
-    //random list of numbers for each vector for proofing
-    vector<double>list1{100, 100, 50, 11, 10, 80, 90, 10, 200};
-    vector<double>list2{1200, 1200, 20, 50.50};
-    auto &result = comp_vec(list1, list2);
+double comp_vec(const vector<double>&v1, const vector<double>&v2)
+{
+    if (v1.empty() || v2.empty() || v1.size() <= 1 || v2.size() <= 1) 
+    {
+        throw std::runtime_error("EMPTY VECTOR(s) (Both Vectors need"
+                "a some numarical value), or not correct amount "
+                "of numbers to calculate average with. ");
+    }
+    double v1_cnt = 0, v2_cnt = 0; //declare v1 & v2 to hold total additions
+    double v1_tot = 0, v2_tot = 0; //declare v1_t and v2_t to hold total avg's.
     
-    cout << result << endl;
-    return 0;
+    auto j = 0;                    //J is to hold count for separate vector.    
+    auto i = 0;                    //same for I as for J :) otherwise division is off
+    
+    for (i = 0; i != v1.size() && j != v2.size(); ++j, ++i) 
+    {
+        v1_cnt += v1[i];
+        v2_cnt += v2[j];
 
+    }
+    //get avg's form both vectors.
+    v1_tot = v1_cnt / i;
+    v2_tot = v2_cnt / j;
+    
+    //return the vector that has largest avg.
+    return v1_tot > v2_tot ? v1_tot : v2_tot;
+}
+int main() 
+{
+    //random list of numbers for each vector for proofing
+    
+    vector< double >list1{2, 7.02506, 22.1003, 47.2256, 82.401, 127.627, 182.902,
+                            248.228, 323.604, 409.03, 504.506, 610.033, 32.22, 44.11};
+    
+    vector< double >list2{2.30256, 3.58828, 5.44542, 7.87399, 10.874, 14.4454, 
+                            18.5883, 23.3026, 28.5883, 34.4454, 40.874, 47.874, 44.5};
+
+    double result = comp_vec(list1, list2);
+    cout << result << endl << endl;
+    return_some_numbers();
+    return 0;
 }
 
