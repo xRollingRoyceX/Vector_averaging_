bfc231ed036d76412dc9d47951bced1cd73dc344
Sean <jesky.sean@outlook.com>
2017-11-29 07:48:33 -0700

    Update Ref_Passing_vector.cpp
    moving function {return_some_numbers} to separate page. Use it if you wish to use some new numbers between your choice.

--- Ref_Passing_vector.cpp
+++ Ref_Passing_vector.cpp
@@ -7,26 +7,7 @@
  */
 #include <inclds>
 #include<exception>
-//[i like this equation for a stream of numbers]
-//  i / (32 + 7) + 2 + (i * i) / 3 << endl;
 
-void return_some_numbers()
-{
-    /* 
-     * change a few numbers and stuff to get some "random" numbers 
-     * to use as testing numbers for the function -
-     * "comp_vec(const &vec, const vec&);" 
-     * using this to just place into calculator (in linux of course to easily check numbers;
-    */
-    /*
-    for (int i = 0; i != 12; ++i) {
-        //cout << i + 0.1 / (32 + 7) + 2.3 + (i * i) / 3.5 << ", ";
-        //cout << i + 0.1 / (32 + 7) + 2.3 + (i * i) / 3.5 << " + ";
-        //cout << i * i / (32.8 + 7.1) + 2 + (i * i) / 0.2 << ", ";
-        cout << i * i / (32.8 + 7.1) + 2 + (i * i) / 0.2 << " + ";
-    }
-     */
-}
 //Get two vectors that have > 2 numbers in each vector.
 double &comp_vec(const vector<double> &v1, const vector<double> &v2) 
 {
