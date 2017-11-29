0c6b48b387dff2e63916dc9b1dc2c565bcd9b20a
Sean <jesky.sean@outlook.com>
2017-11-29 09:24:38 -0700

    Update create_some_numbers.cpp
    fixed comments.

--- create_some_numbers.cpp
+++ create_some_numbers.cpp
@@ -1,21 +1,26 @@
 //[i like this equation for a stream of numbers]
 //  i / (32 + 7) + 2 + (i * i) / 3 << endl;
-#include"ref-nbr-vec"
 
+#include"ref-nbr-vec"
 void return_some_numbers()
 {
-    /* 
+     /* i like this equation for a stream of numbers]
+     *           cout << i / (32 + 7) + 2 + (i * i) / 3 << endl;
+     *
      * change a few numbers and stuff to get some "random" numbers 
      * to use as testing numbers for the function -
      * "comp_vec(const &vec, const vec&);" 
-     * using this to just place into calculator (in linux of course to easily check numbers;
+     *
+     * remove comments here to use return_some_numbers -
+     *  using this to just get, and place quasi-random values into 
+     *  a calculator to help make sure the function is correct. 
+     *  (linux calculator of course to easily check numbers );
     */
-    /*
+    
     for (int i = 0; i != 12; ++i) {
         //cout << i + 0.1 / (32 + 7) + 2.3 + (i * i) / 3.5 << ", ";
         //cout << i + 0.1 / (32 + 7) + 2.3 + (i * i) / 3.5 << " + ";
         //cout << i * i / (32.8 + 7.1) + 2 + (i * i) / 0.2 << ", ";
-        cout << i * i / (32.8 + 7.1) + 2 + (i * i) / 0.2 << " + ";
+        //cout << i * i / (32.8 + 7.1) + 2 + (i * i) / 0.2 << " + ";
     }
-     */
 }
