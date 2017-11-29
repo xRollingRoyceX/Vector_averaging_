d907610b8ca2d9725ff33f224559ea48a2f85e8e
Sean <jesky.sean@outlook.com>
2017-11-29 07:51:57 -0700

    Create create_some_numbers.cpp
    moved this function form one page of text to another to more easily separate ideas of code... (still self teaching till i get to classes, then signing up for actual teaching from a teacher.)

---
+++ create_some_numbers.cpp
@@ -0,0 +1,20 @@
+//[i like this equation for a stream of numbers]
+//  i / (32 + 7) + 2 + (i * i) / 3 << endl;
+
+void return_some_numbers()
+{
+    /* 
+     * change a few numbers and stuff to get some "random" numbers 
+     * to use as testing numbers for the function -
+     * "comp_vec(const &vec, const vec&);" 
+     * using this to just place into calculator (in linux of course to easily check numbers;
+    */
+    /*
+    for (int i = 0; i != 12; ++i) {
+        //cout << i + 0.1 / (32 + 7) + 2.3 + (i * i) / 3.5 << ", ";
+        //cout << i + 0.1 / (32 + 7) + 2.3 + (i * i) / 3.5 << " + ";
+        //cout << i * i / (32.8 + 7.1) + 2 + (i * i) / 0.2 << ", ";
+        cout << i * i / (32.8 + 7.1) + 2 + (i * i) / 0.2 << " + ";
+    }
+     */
+}
